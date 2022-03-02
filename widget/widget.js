b076e60173fe0030362fdfc0f8550f1ce18ea38b
cshape <caleshapera@gmail.com>
2022-03-01 19:06:27 -0800

    ok

--- widget/widget.js
+++ widget/widget.js
@@ -8,8 +8,14 @@ var updateCallback = function(data) {
     console.log("stringified new line: " + newLine);
     let newInfoArray = JSON.parse(newLine)
     let usertext = document.getElementById("usertext");
+    let agenttext = document.getElementById("agenttext");
     console.log(newInfoArray[0].text)
-    usertext.innerHTML = newInfoArray[0].text;
+    if (newLine[0].by = "Visitor") {
+        usertext.innerHTML = newLine[0].text;
+    } else {
+        agenttext.innerHTML = newLine[0].text;
+    }
+    // usertext.innerHTML = newInfoArray[0].text;
     // called each time the value is updated.
     // If there's an existing value when bind is called - this callback
     // will be called with the existing value
