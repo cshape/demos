0b2daa4bc619edb970751f4141aa7e83777633d7
cshape <caleshapera@gmail.com>
2022-03-01 19:17:11 -0800

    ok

--- widget/widget.js
+++ widget/widget.js
@@ -1,25 +1,13 @@
-console.log("hello!");
 lpTag.agentSDK.init()
 var updateCallback = function(data) {
-    // Do something with the returning data
-    // var path = data.key;
-    // var newLine = JSON.stringify(data.newValue);
     var newLine = data.newValue;
-    // console.log("DATA: " + JSON.stringify(data));
-    // console.log("stringified new line: " + newLine);
-    // let newInfoArray = JSON.parse(newLine)
     let usertext = document.getElementById("usertext");
     let agenttext = document.getElementById("agenttext");
-    console.log(newLine[0].text)
     if (newLine[0].by = "Visitor") {
-        usertext.innerHTML = newLine[0].text;
+        usertext.innerHTML == newLine[0].text;
     } else {
-        agenttext.innerHTML = newLine[0].text;
+        agenttext.innerHTML == newLine[0].text;
     }
-    // usertext.innerHTML = newInfoArray[0].text;
-    // called each time the value is updated.
-    // If there's an existing value when bind is called - this callback
-    // will be called with the existing value
 };
 
 var notifyWhenDone = function(err) {
