7740aaab53a88c17c8a3b503f923f791e25020a0
cshape <caleshapera@gmail.com>
2022-03-01 17:34:49 -0800

    ok

--- widget/widget.js
+++ widget/widget.js
@@ -1,8 +1,12 @@
-console.log("hello!")
+console.log("hello!");
 var updateCallback = function(data) {
     // Do something with the returning data
-    var path = data.key;
+    // var path = data.key;
     var value = data.newValue;
+    console.log("DATA: " + data);
+    console.log("VALUE: " + value);
+    let usertext = document.getElementById("usertext");
+    usertext.innerHTML = value;
     // called each time the value is updated.
     // If there's an existing value when bind is called - this callback
     // will be called with the existing value
@@ -10,10 +14,9 @@ var updateCallback = function(data) {
 
 var notifyWhenDone = function(err) {
     if (err) {
-        // Do something with the error
+        console.log(err);
     }
-    // called when the bind is completed successfully,
-    // or when the action terminated with an error.
+    console.log("set the value");
 };
 var pathToData = "chatTranscript.lines";
 lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
