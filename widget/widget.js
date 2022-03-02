1ca3fc40076742850a9bcb3153044dce0f893cc3
cshape <caleshapera@gmail.com>
2022-03-01 18:43:56 -0800

    ok

--- widget/widget.js
+++ widget/widget.js
@@ -3,15 +3,13 @@ lpTag.agentSDK.init()
 var updateCallback = function(data) {
     // Do something with the returning data
     // var path = data.key;
-    var newLine = data.newValue;
+    var newLine = JSON.stringify(data.newValue);
     console.log("DATA: " + JSON.stringify(data));
-    console.log("VALUE: " + JSON.stringify(newLine));
+    console.log("stringified new line: " + newLine);
+    let newInfoArray = JSON.parse(newLine)
     let usertext = document.getElementById("usertext");
-    let textFromSDK = ""
-    data.map(line => {
-        textFromSDK = textFromSDK + line.text
-    })
-    usertext.innerHTML = textFromSDK;
+    console.log(newInfoArray[0].text)
+    usertext.innerHTML = newInfoArray[0].text;
     // called each time the value is updated.
     // If there's an existing value when bind is called - this callback
     // will be called with the existing value
@@ -25,3 +23,4 @@ var notifyWhenDone = function(err) {
 };
 var pathToData = "chatTranscript.lines";
 lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
+
