7043f0808a5144cac151ab628a6a62eff5812056
cshape <caleshapera@gmail.com>
2022-06-16 15:26:26 +0200

    widget stuff

--- widget/widget.js
+++ widget/widget.js
@@ -1,5 +1,6 @@
 lpTag.agentSDK.init()
 var updateCallback = function(data) {
+    // get convo lines as they happen
     var newLine = data.newValue;
     let usertext = document.getElementById("usertext");
     let agenttext = document.getElementById("agenttext");
@@ -7,6 +8,11 @@ var updateCallback = function(data) {
         usertext.innerHTML = newLine[0].text;
     } else {
         agenttext.innerHTML = newLine[0].text;
+    }
+    var ccsData = document.getElementById("ccs").innerHTML;
+    console.log(ccsData)
+    if (ccsData == "") {
+        // API call to grab data
     }
 };
 
