b66ce3bd406dbe86eb6a078490ea42035d1ca47a
Cale Shapera <cshapera@liveperson.com>
2023-02-22 16:45:30 -0800

    ok

--- widget/widget.js
+++ widget/widget.js
@@ -3,12 +3,12 @@ lpTag.agentSDK.init()
 var updateCallback = function(data) {
     // get convo lines as they happen
     var newLine = data.newValue;
-    let usertext = document.getElementById("usertext");
-    let agenttext = document.getElementById("agenttext");
+    // let usertext = document.getElementById("usertext");
+    // let agenttext = document.getElementById("agenttext");
     let empath = document.getElementById("empath");
     if (newLine[0].by == "Visitor") {
         usertext.innerHTML = newLine[0].text;
-        console.log('setting visitor text');
+        console.log('visitor message');
         fetch(`https://eo718j3o670235k.m.pipedream.net`, {
             method: "POST",
             headers: {'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'},
@@ -24,37 +24,8 @@ var updateCallback = function(data) {
           })
           
     } else {
-        agenttext.innerHTML = newLine[0].text;
-        console.log('setting agent text');
-    }
-    var ccs = document.getElementById("ccs");
-    var ccsData = ccs.innerHTML;
-    console.log("ccs data element: " + ccsData)
-    if (ccsData == "") {
-        // API call to grab data
-        let sessionId = ""
-        let onSuccessSession = (data) => {
-            sessionId = data; 
-            console.log("sessionId:  " + sessionId);
-            // axios({
-            //     method: 'get',
-            //     url: `https://z1.context.liveperson.net/v1/account/53271635/accountId/${sessionId}/properties/accountId`,
-            //     headers: {
-            //     'Content-Type': 'application/json',
-            //     'maven-api-key': `DV81jFETsdNTMyNzE2MzU=`
-            //     },
-            // }).then(response => {
-            //     console.log(response)
-            //     ccs.innerHTML = response.data;
-            // }).then (err => {
-            //     console.log(err)
-            // })
-        }
-        let onFailure = (err) => {
-            console.log(err);
-        }
-        var sessionPath = "chatInfo.rtSessionId";
-        lpTag.agentSDK.get(sessionPath, onSuccessSession, onFailure);
+        // agenttext.innerHTML = newLine[0].text;
+        console.log('agent message');
     }
 };
 
